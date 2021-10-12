c540e3362428b437a2f9457312ad0419d37b86f2
Azamat <azamatmove@gmail.com>
2021-10-12 11:51:56 +0600

    Initial Commit

---
+++ compendiium6/main.swift
@@ -0,0 +1,49 @@
+//import Darwin
+//var text = readLine()!
+//var array = text.split(separator: " ")
+//
+//var maxText = ""
+//
+//array.forEach { item in
+//    if maxText.count < item.count {
+//        maxText = String(item)
+//    }
+//}
+//
+//for (index, item) in array.enumerated() {
+//    if item == maxText {
+//        array.remove(at: index)
+//
+//        break
+//    }
+//}
+//var textResult = ""
+//
+//array.forEach { item in
+//    if textResult.count == 0 {
+//    textResult = textResult + item
+//    } else {
+//        textResult = textResult + " " + item
+//    }
+//}
+//print(textResult)
+
+
+
+
+var array = [1, -2, 30, -20, 20, 10]
+var positive: [Int] = []
+var negative: [Int] = []
+
+array.forEach { item in
+    if item > 0 {
+        positive.append(item)
+    } else {
+        negative.append(item)
+    }
+}
+
+print(positive)
+print(negative)
+
+
